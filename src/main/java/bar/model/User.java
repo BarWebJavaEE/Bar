29ed6498e9404ae10ddffda1235df32297c5342e
bgmitkov <ropsan@abv.bg>
2016-09-02 14:31:15 +0300

    persistence change

--- src/main/java/bar/model/User.java
+++ src/main/java/bar/model/User.java
@@ -2,35 +2,38 @@ package entity;
 
 import java.io.Serializable;
 import javax.persistence.*;
-import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
 
+import java.util.Date;
+import java.util.List;
 
 /**
  * The persistent class for the user database table.
  * 
  */
 @Entity
-@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+@XmlRootElement
+/* @NamedQuery(name="User.findAll", query="SELECT u FROM User u") */
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
 	private String userName;
 
 	private String password;
 
-	//bi-directional many-to-many association to Role
+	private String email;
+
+	@Temporal(TemporalType.DATE)
+	private Date dateOfBirth;
+
+	// bi-directional many-to-many association to Role
 	@ManyToMany
-	@JoinTable(
-		name="users_roles"
-		, joinColumns={
-			@JoinColumn(name="UserName")
-			}
-		, inverseJoinColumns={
-			@JoinColumn(name="RoleName")
-			}
-		)
+	@JoinTable(name = "users_roles", joinColumns = { @JoinColumn(name = "UserName") }, inverseJoinColumns = {
+			@JoinColumn(name = "RoleName") })
 	private List<Role> roles;
 
 	public User() {
